45edc32edfe61c0a5d68e125527f0530a9a6932b
Ridwan Sidik <ridwan@upwardstech.com>
2016-10-26 16:41:20 +0700

    add language file

--- test/karma.conf.js
+++ test/karma.conf.js
@@ -48,6 +48,12 @@ module.exports = function(config) {
       'bower_components/angular-ui-codemirror/ui-codemirror.js',
       'bower_components/moment/moment.js',
       'bower_components/angular-moment/angular-moment.js',
+      'bower_components/angular-translate/angular-translate.js',
+      'bower_components/angular-dynamic-locale/src/tmhDynamicLocale.js',
+      'bower_components/angular-translate-loader-static-files/angular-translate-loader-static-files.js',
+      'bower_components/angular-translate-storage-cookie/angular-translate-storage-cookie.js',
+      'bower_components/angular-translate-storage-local/angular-translate-storage-local.js',
+      'bower_components/angular-translate-handler-log/angular-translate-handler-log.js',
       'bower_components/angular-mocks/angular-mocks.js',
       // endbower
       'app/scripts/**/*.js',
