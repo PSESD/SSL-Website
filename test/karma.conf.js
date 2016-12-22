090fcf815f22ffe5ed5747b9f2ce44cae4cf9090
Devin Crandall <devin@thehiltagency.com>
2016-12-22 15:55:22 -0800

    student detail data chart

--- test/karma.conf.js
+++ test/karma.conf.js
@@ -56,6 +56,8 @@ module.exports = function(config) {
       'bower_components/angular-translate-handler-log/angular-translate-handler-log.js',
       'bower_components/chart.js/dist/Chart.js',
       'bower_components/angular-chart.js/dist/angular-chart.js',
+      'bower_components/requirejs/require.js',
+      'bower_components/jquery.easy-pie-chart/dist/jquery.easypiechart.js',
       'bower_components/angular-mocks/angular-mocks.js',
       // endbower
       'app/scripts/**/*.js',
