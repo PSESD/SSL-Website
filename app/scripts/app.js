de667bbd251bade91d37e1e4bbf11824999f05d1
Devin Crandall <devin@thehiltagency.com>
2017-03-23 13:14:17 -0700

    Login/out of extra tabs on logout.

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -183,6 +183,13 @@
       window.history.back();
     };
 
+    function storageChange(event) {
+      if (event.key === 'id') {
+         location.reload();
+      }
+    }
+    window.addEventListener('storage', storageChange, false);
+
   }
 
 
